1dc8713dfb846f58f5cbeb552325454c47bca420
abdulsalamcodes <abdulsalamabodunrin369@gmail.com>
2022-01-25 21:47:21 +0100

    fix: ignore more files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e199dd8331e2dad5"; }
+/******/ 		__webpack_require__.h = function() { return "13fd1ac5f647ac2e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
